10e745890de62d997125e0a5b5eb44ddbf79aff4
bryan803 <bryananthony654@gmail.com>
2025-12-23 20:21:51 +0700

    Force HTTPS on Laravel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\Donation;
 use App\Observers\DonationObserver;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Donation::observe(DonationObserver::class);
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
 
